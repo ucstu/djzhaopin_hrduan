c2ebcf4943ff094fedeac5a9bfcacf0ebef9e015
zxd2219 <2219454275@qq.com>
2022-04-14 09:46:44 +0800

    初始提交

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-standard-scss",
+    "stylelint-config-recommended-vue",
+    "stylelint-config-recess-order",
+    "stylelint-config-prettier",
+  ]
+}
---
+++ src/pages/Manage/Message/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="content">1111</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.content {
+  width: 100%;
+  height: 100%;
+  background-color: aqua;
+}
+</style>
