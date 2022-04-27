7b3025db4b947945680aca97f76b15b3ac9521c2
leben <1979044994@qq.com>
2022-04-27 04:11:37 +0000

    添加开发时检查

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import ElementPlus from "unplugin-element-plus/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
@@ -22,6 +24,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    checker({ typescript: true }),
     AutoImport({
       resolvers: [
         ElementPlusResolver({
