d6899c980c50f32a27136ab46467992b6bab12cc
张旭东 <2219454275@qq.com>
2022-05-20 18:59:17 +0000

    ceshi

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,12 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
+  readonly VITE_CDN_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
